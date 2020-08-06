cc1cb8f3d65c6784fd6d43d663301bebdf864dd8
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-08-06 12:42:46 +0600

    Added all imports from components, added components to html, added radio buttons and state

--- cafe-bill-counter/src/containers/App.js
+++ cafe-bill-counter/src/containers/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import EquallyInterface from '../components/FirstMode/EquallyInterface/EquallyInterface';
+import EachPersonInterface from '../components/SecondMode/EachPersonInterface/EachPersonInterface'
 
-const App = () =>{
+const App = () => {
+
+  let [value,setValue] = useState('');
+
+  const getPorovnu = () =>{
+    value = 'first';
+    setValue(value);
+  }
+
+  const getEach = () =>{
+    value = 'second';
+    setValue(value);
+  }
+
+  let porovnuMode = null;
+  let eachMode = null
+  if(value === 'first'){
+    porovnuMode = <EquallyInterface/>;
+  }
+  if(value === 'second'){
+    eachMode = <EachPersonInterface/>
+  }
   return (
     <div className="App">
-     
+      <h3>Сумма заказа считается :</h3>
+      <p><input type="radio" name="value" checked={value === 'first'} onChange={getPorovnu}/>Поровну между всеми участниками</p>
+      <p><input type="radio" name="value" checked={value === 'second'} onChange={getEach}/>Каждому индивидуально</p>
+
+      {porovnuMode}
+      {eachMode}
     </div>
   );
 }
